2945efb03ad2065f1f4677722b4c209676eec47a
Maxime Rouiller <angelzerosoft@gmail.com>
2017-07-12 12:48:44 -0400

    built-in tuples?

--- src/hello/main.go
+++ src/hello/main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 )
 
-/// test
-/* testing some more*/
+// because I need a function for that.
 func add(x, y int) int {
 	return x + y
+}
+
+// Hello tuples? Nice built-in feature.
+func swap(x, y string) (string, string) {
+	return y, x
 }
 
 func main() {
 	fmt.Println("Welcome to the playground!")
 	fmt.Println("Testing an add function ", add(2, 4))
+	a, b := swap("world", "hello")
+	fmt.Println("Apparently, we love tuples? ", a, b)
 }
