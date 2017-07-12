3ea7fad4733d76f66e812dcabe57a4201d91a11c
Maxime Rouiller <angelzerosoft@gmail.com>
2017-07-12 14:10:50 -0400

    apis fun

--- build.cmd
+++ build.cmd
@@ -1,4 +1,6 @@
 @echo off
 
+go get -u github.com/gorilla/mux
+
 REM go build ./src/hello
 go build ./src/simple-api
--- src/simple-api/main.go
+++ src/simple-api/main.go
@@ -2,15 +2,24 @@ package main
 
 import (
 	"fmt"
-	"html"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
-		fmt.Fprintf(w, "Hello from golang on path %q", html.EscapeString(req.URL.Path))
-	})
+	// http.HandleFunc("/", func(w http.ResponseWriter, req *http.Request) {
+	// 	fmt.Fprintf(w, "Hello from golang on path %q", html.EscapeString(req.URL.Path))
+	// })
+	//
+	// log.Fatal(http.ListenAndServe(":5000", nil))
 
-	log.Fatal(http.ListenAndServe(":5000", nil))
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/", Index)
+	log.Fatal(http.ListenAndServe(":5000", router))
+}
+
+func Index(w http.ResponseWriter, request *http.Request) {
+	fmt.Fprintf(w, "Hello again from golang!")
 }
