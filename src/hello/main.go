1657e85c53b26b85c422f53ef875572bb2fe3f6f
Maxime Rouiller <angelzerosoft@gmail.com>
2017-07-12 12:53:07 -0400

    more fun with tuples

--- src/hello/main.go
+++ src/hello/main.go
@@ -14,9 +14,17 @@ func swap(x, y string) (string, string) {
 	return y, x
 }
 
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
 	fmt.Println("Welcome to the playground!")
 	fmt.Println("Testing an add function ", add(2, 4))
 	a, b := swap("world", "hello")
 	fmt.Println("Apparently, we love tuples? ", a, b)
+
+	fmt.Println(split(17))
 }
